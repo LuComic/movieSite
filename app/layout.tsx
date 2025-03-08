9f5c3c2d079345f9c3578028d5e36122f6530ba1
Lukas Jääger <81866926+LuComic@users.noreply.github.com>
2025-03-08 16:21:13 +0200

    backgrond color

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="bg-black">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
       >
         <MainLayout>{children}</MainLayout>
       </body>

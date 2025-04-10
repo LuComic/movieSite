1a1c40023fb87a593a9f2b7da3cc587691e28f71
proovend <proovendone@proton.me>
2025-04-10 11:58:09 +0300

    build portable

--- movieSiteBack/mix.exs
+++ movieSiteBack/mix.exs
@@ -3,7 +3,7 @@ defmodule MovieSiteBack.MixProject do
 
   def project do
     [
-      app: :movieSiteBack,
+      app: :moviesiteback,
       version: "0.1.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
